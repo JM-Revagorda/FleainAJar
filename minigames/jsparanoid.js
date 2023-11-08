7f1937e58dae72f4f6be25e31b75eb75d1ba72e7
JM Revagorda <jmcr011504@gmail.com>
2023-11-08 21:27:23 +0800

    Minigame Alpha 3
    Added animation systems and bacground image in jsparanoid. Thanks Rianne for making background and animation!

--- minigames/jsparanoid.js
+++ minigames/jsparanoid.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function(){
     const ctx = canvas.getContext('2d');
     canvas.width = 2000;
     canvas.height = 720;
-    let timer = 1500;
+    let timer = 5000;
     let entities = [];
     let gameOver = false;
     let score = 0;
@@ -12,12 +12,15 @@ window.addEventListener('load', function(){
         constructor(){
             this.keys = [];
             window.addEventListener('keydown', e => {
-                if (e.key === 'ArrowUp' && this.keys.indexOf(e.key) === -1){
+                if ((e.key === 'ArrowUp' ||
+                     e.key == " ")
+                     && this.keys.indexOf(e.key) === -1){
                     this.keys.push(e.key);
                 }
             });
             window.addEventListener('keyup', e => {
-                if (e.key === 'ArrowUp'){
+                if (e.key === 'ArrowUp' ||
+                    e.key == " "){
                     this.keys.splice(this.keys.indexOf(e.key), 1);
                 }
             });
@@ -28,11 +31,18 @@ window.addEventListener('load', function(){
         constructor(gameWidth, gameHeight){
             this.gameWidth = gameWidth;
             this.gameHeight = gameHeight;
-            this.width = 200;
-            this.height = 200;
+            this.width = 260;
+            this.height = 348;
             this.x = this.gameWidth / 2 * 1.1;
             this.y = this.gameHeight - this.height;
             this.color = 'black';
+            this.frameX = 0;
+            this.frameY = 0; //Temporary, until we get a new state
+            this.fps = 9;
+            this.frameTimer = 0;
+            this.frameInterval = 1000/this.fps;
+            this.maxFrame = 3;
+            this.image = document.getElementById("objPlayer");
             // this.speed = 5;
         }
         draw(context){
@@ -40,23 +50,75 @@ window.addEventListener('load', function(){
             //drawImage(image *must have an <img> tag*, source x, source y, source width, source height, destx, desty, destwidth, destheight) - creates the sprites we know of today
             context.fillStyle = "black";
             context.fillRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, 0 * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
         }
-        update(input, context){
+        update(input, deltaTime){
             if(input.keys.indexOf("ArrowUp") > -1){
                 // this.speed = 0;
-                this.color = 'red';
+                // this.color = 'red';
                 score += 0;
             } else {
                 // this.speed = 5;
-                this.color = 'blue';
+                // this.color = 'blue';
                 score += 3;
             }
 
-            context.fillStyle = this.color;
-            context.fillRect(this.x, this.y, this.width, this.height);
+            //Animation
+            if(this.frameTimer > this.frameInterval){
+                if (this.frameX >= this.maxFrame){ this.frameX = 0;}
+                else {
+                    if(input.keys.indexOf("ArrowUp") == -1) this.frameX++;
+                    else this.frameX = 0;
+                }
+                this.frameTimer = 0;
+            } else {
+                this.frameTimer += deltaTime;
+            }
+
+            // context.fillStyle = this.color;
+            // context.fillRect(this.x, this.y, this.width, this.height);
         }
     }
 
+    class Background{
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.image = document.getElementById("objBackground");
+            this.x = 0;
+            this.y = 0;
+            this.width = 4031;
+            this.height = 744;
+            this.speed = 7;
+        }
+        draw(context){
+            context.drawImage(this.image, this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.x + this.width - this.speed, this.y, this.width, this.height);
+        }
+        update(input){
+            this.x -= this.speed;
+            if (this.x < 0 - this.width) this.x = 0;
+
+            if(input.keys.indexOf("ArrowUp") > -1) this.speed = 0;
+            else this.speed = 7;
+        }
+    }
+
+    class Dark{
+        constructor(gameHeight, gameWidth){
+            this.gameHeight = gameHeight;
+            this.gameWidth = gameWidth;
+            this.width = this.gameWidth;
+            this.height = this.gameHeight;
+            this.opacity = 0;
+            this.x = 0;
+            this.y = 0;
+        }
+        draw(context){
+            context.fillStyle = "black";
+            context.fillStroke(this.x, this.y, this.width, this.height);
+        }
+    }
     // class Eye{
     //     constructor(gameWidth, gameHeight){
     //         this.gameWidth = gameWidth;
@@ -97,14 +159,20 @@ window.addEventListener('load', function(){
     // }
 
     let steps = 0;
-    function Timer(entity){
-        if(entity.color != "red"){
+    function Timer(input){
+        // if(entity.color != "red"){
+        //     timer -= steps + 1;
+        // }
+        // else{
+        //     timer += 3;
+        // }
+
+        if(input.keys.indexOf("ArrowUp") > -1){
+            timer += 3;
+        } else {
             timer -= steps + 1;
         }
-        else{
-            timer += 3;
-        }
-        if(timer > 1500) timer = 1500;
+        if(timer > 5000) timer = 5000;
         //Game Over mechanic
         else if(timer < 0) gameOver = true;
 
@@ -113,14 +181,14 @@ window.addEventListener('load', function(){
 
     function displayText(context, gameWidth, gameHeight){
         context.font = "20px Arial";
-        context.fillStyle = "black";
+        context.fillStyle = "white";
         context.fillText("Timer: " + timer, 20, 50);
         context.font = "20px Arial";
-        context.fillStyle = "black";
+        context.fillStyle = "white";
         context.fillText("Score: " + Math.round(score * 0.4), 20, 100);
         if(gameOver){
             context.font = "40px Arial";
-            context.fillStyle = "black";
+            context.fillStyle = "white";
             context.fillText("Game Over!", gameWidth / 2, gameHeight / 2);
             console.log(score);
         }
@@ -143,6 +211,7 @@ window.addEventListener('load', function(){
 
     const input = new InputHandler();
     const player = new Player(canvas.width, canvas.height);
+    const background = new Background(canvas.width, canvas.height);
     // const eye = new Eye(canvas.width, canvas.height);
 
     // let lastTime = 0;
@@ -150,16 +219,22 @@ window.addEventListener('load', function(){
     // let entityInterval = 500;
     // let randomEntityInterval = Math.random() * 100 + 20;
 
-    function animate(){
-        Timer(player);
+    let lastTime = 0;
+
+    function animate(timeStamp){
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        Timer(input);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        background.draw(ctx);
+        background.update(input);
         player.draw(ctx);
-        player.update(input, ctx);
+        player.update(input, deltaTime);
         // handleEnemies(deltaTime);
         // eye.draw(ctx);
         // eye.update(input);
         displayText(ctx, canvas.width, canvas.height);
         if(!gameOver){requestAnimationFrame(animate);}
     }
-    animate();
+    animate(0);
 });
