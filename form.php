557118d2fabac52ad2c15b1f225f310ecf2eb64a
JM-Revagorda <144304545+JM-Revagorda@users.noreply.github.com>
2023-12-07 10:23:00 +0800

    Beta Patch 11
    Starting to implement php systems

---
+++ form.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <title>Flea in a Jar</title>
+        <style>
+            table{
+                margin: auto;
+            }
+        </style>
+    </head>
+<body>
+    <form action="form.php" method="get">
+        <table>
+            <tr>
+                <td colspan="5"><h1>How do you feel today?</h1></td>
+            </tr>
+            <tr>
+                <td>
+                    Super!<br/>
+                    <input type="radio" name="emotion" value="great">
+                </td>
+                <td>
+                    OK Lang<br/>
+                    <input type="radio" name="emotion" value="alright">
+                </td>
+                <td>
+                    Dele Lang Sa<br/>
+                    <input type="radio" name="emotion" value="a feeling I rather not say">
+                </td>
+                <td>
+                    Kapoy na<br/>
+                    <input type="radio" name="emotion" value="not that great">
+                </td>
+                <td>
+                    YYAAAAWWWAAAAA!!!!<br/>
+                    <input type="radio" name="emotion" value="SSSHHHHIIIITTTTTT!!!!!!">
+                </td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="submit"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
